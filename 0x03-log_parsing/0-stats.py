c4075771dcd366c1a7b15f5c2f744fc9ddd02a11
paulagbelex <paulagbelex@gmail.com>
2023-10-19 14:36:42 +0000

    corrected indentation error and pep8 error

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -14,7 +14,7 @@ def printsts(dic, size):
 
 
 sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-           "404": 0, "405": 0, "500": 0}
+       "404": 0, "405": 0, "500": 0}
 
 count = 0
 size = 0
@@ -29,13 +29,13 @@ try:
 
         try:
             size += int(stlist[-1])
-        except:
+        except ValueError:
             pass
 
         try:
             if stlist[-2] in sts:
                 sts[stlist[-2]] += 1
-        except:
+        except IndexError:
             pass
     printsts(sts, size)
 
